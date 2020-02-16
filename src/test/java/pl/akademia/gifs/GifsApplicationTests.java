1a0d0c050f748a4efd8b0faa8c337eaea65903e0
Iga Hofman <iga.hofman@gmail.com>
2020-02-16 11:00:51 +0100

    First commit

---
+++ src/test/java/pl/akademia/gifs/GifsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademia.gifs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
