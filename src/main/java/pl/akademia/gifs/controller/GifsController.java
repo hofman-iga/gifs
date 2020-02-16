7d3c72ade263bd6d76fda3c328969a03c9e99c15
Iga Hofman <iga.hofman@gmail.com>
2020-02-16 11:26:26 +0100

    Added test controller

---
+++ src/main/java/pl/akademia/gifs/controller/GifsController.java
@@ -0,0 +1,19 @@
+package pl.akademia.gifs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class GifsController {
+
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello(){
+        return "Hello";
+    }
+
+}
